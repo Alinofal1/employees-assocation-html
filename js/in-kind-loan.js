d6bb7e588fc2610397c595cbddfde84e723a952d
Alinofal1 <alinofal405@gmail.com>
2025-07-09 08:55:20 +0300

    in kind loan edits

--- js/in-kind-loan.js
+++ js/in-kind-loan.js
@@ -3,42 +3,42 @@ const newsData = [
   {
     title: "عنوان الخبر ",
     date: "12 حزيران 2025",
-    img: "../assets/images/cardsImg.png",
-    desc: `تمويل لشراء احتياجاتك من شركائنا (تأمين المركبات، أجهزة كهربائية وإلكترونية، أثاث منزلي، صيانة ودهان سيارات، خدمات طبية للأسنان، محروقات…) وتقسيط يصل حتى 12 شهراً
+    img: "/assets/images/cardsImg.png",
+    desc: `تمويل لشراء احتياجاتك من شركائنا (تأمين المركبات، خدمات طبية للأسنان، محروقات…) وتقسيط يصل حتى 12 شهراً
 كهربائية وإلكترونية، أثاث منزلي، صيانة ودهان سيارات، خدمات...`,
   },
   {
     title: "عنوان الخبر ",
     date: "15 حزيران 2025",
-    img: "../assets/images/cardsImg.png",
-    desc: `تمويل لشراء احتياجاتك من شركائنا (تأمين المركبات، أجهزة كهربائية وإلكترونية، أثاث منزلي، صيانة ودهان سيارات، خدمات طبية للأسنان، محروقات…) وتقسيط يصل حتى 12 شهراً
+    img: "/assets/images/cardsImg.png",
+    desc: `تمويل لشراء احتياجاتك من شركائنا (تأمين المركبات، خدمات طبية للأسنان، محروقات…) وتقسيط يصل حتى 12 شهراً
 كهربائية وإلكترونية، أثاث منزلي، صيانة ودهان سيارات، خدمات...`,
   },
   {
     title: "عنوان الخبر ",
     date: "20 حزيران 2025",
-    img: "../assets/images/cardsImg.png",
-    desc: `تمويل لشراء احتياجاتك من شركائنا (تأمين المركبات، أجهزة كهربائية وإلكترونية، أثاث منزلي، صيانة ودهان سيارات، خدمات طبية للأسنان، محروقات…) وتقسيط يصل حتى 12 شهراً
+    img: "/assets/images/cardsImg.png",
+    desc: `تمويل لشراء احتياجاتك من شركائنا (تأمين المركبات، خدمات طبية للأسنان، محروقات…) وتقسيط يصل حتى 12 شهراً
 كهربائية وإلكترونية، أثاث منزلي، صيانة ودهان سيارات، خدمات...`,
   },
 ];
 
-const inKindCards = document.getElementById("in-kind-cards");
+const newsCards = document.getElementById("in-kind-cards");
 
 newsData.forEach((item) => {
   const card = document.createElement("div");
-  card.className = "in-kindContent";
+  card.className = "newsCardsContent";
   card.innerHTML = `
-    <a href="#" class="in-kindLink">
-      <img src="${item.img}" alt="${item.title}" class="in-kindImage" />
-      <div class="in-kindText">
+    <a href="#" class="newsCardLink">
+      <img src="${item.img}" alt="${item.title}" class="newsCardsImage" />
+      <div class="newsCardText">
         <h1>${item.title}</h1>
         <p class="date">${item.date}</p>
-        <p class="in-kind-Para">
+        <p class="newsPara">
           ${item.desc} <span>معرفة المزيد</span>
         </p>
       </div>
     </a>
   `;
-  inKindCards.appendChild(card);
+  newsCards.appendChild(card);
 });
