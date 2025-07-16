a48a759cc4def9566b2b62afbd41a6d836492b08
Alinofal1 <alinofal405@gmail.com>
2025-07-16 15:13:53 +0300

    newinnerpages

--- js/newInnerPages.js
+++ js/newInnerPages.js
@@ -17,17 +17,23 @@ newsData.forEach((item, index) => {
   img.className = "media-img";
   wrapper.appendChild(img);
 
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("class", "svg-overlay");
+  svg.setAttribute("viewBox", "0 0 24 24");
+
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
   if (index === 1) {
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("class", "svg-overlay");
-    svg.setAttribute("viewBox", "0 0 24 24");
-
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.setAttribute("d", "M8 5v14l11-7z");
-    svg.appendChild(path);
-
-    wrapper.appendChild(svg);
+  } else {
+    path.setAttribute(
+      "d",
+      "M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"
+    );
   }
+
+  svg.appendChild(path);
+  wrapper.appendChild(svg);
 
   grid.appendChild(wrapper);
 });
